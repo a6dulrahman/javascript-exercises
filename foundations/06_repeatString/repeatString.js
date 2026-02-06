7cd79ef9d38fb18506858974b729f934614f1b90
a6dulrahman <225739262+abdulra6man@users.noreply.github.com>
2026-02-06 11:28:49 +0200

    Finish 06_RepeatString exercise
    😊

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,28 @@
-const repeatString = function() {
 
+const repeatString = function (str, count)
+{
+    // count == 0 -> ''
+    // count == 1 -> 'str'
+    // count == num -> 'str' * num
+    // count == negative -> 'ERROR'  
+    let result = str;
+
+    if (count < 0)
+    {
+        return "ERROR";
+
+    } else if (count === 0)
+    {
+        return "";
+
+    } else
+    {
+        for (let i = 1; i < count; i++) 
+        {
+            result += str;
+        };
+        return result;
+    };
 };
 
 // Do not edit below this line
