7cac070836c8b308e18b83017c72ffeb1c29680f
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-07 13:20:43 +0200

    Finish 08_removeFromArray
    😊

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...theArgs)
+{
+    for (let item of theArgs) 
+    {
+        while (array.includes(item))
+        {
+            array.splice(array.indexOf(item), 1);
+
+        };
+
+    };
+
+    return array;
+
 };
 
 // Do not edit below this line
