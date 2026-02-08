8bcbafe362676574024ec2b70acfdedb54dd8e9d
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-08 11:59:17 +0200

    Finish 09_sumAll exercise
    😊

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+
+const sumAll = function (start, end) 
+{
+    if (Number.isInteger(start) && Number.isInteger(end) && start > 0 && end > 0)
+    {
+        if (start < end)
+        {
+            let result = 0;
+
+            for (let i = start; i <= end; i++) 
+            {
+                result += i;
+
+            };
+
+            return result;
+
+        } else
+        {
+            let result = 0;
+
+            for (let i = end; i <= start; i++) 
+            {
+                result += i;
+
+            };
+
+            return result;
+
+        };
+
+    } else
+    {
+        return "ERROR";
+
+    };
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
