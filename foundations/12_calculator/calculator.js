d2c859a73fb6211ef8684ef7aafac128d99adfcc
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-20 22:45:16 +0200

    Solving 12_calculator
    ☺️

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,73 @@
-const add = function() {
-	
+
+const add = function (a, b)
+{
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b)
+{
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array)
+{
+  if (array.length > 0)
+  {
+    const product = array.reduce((total, current) =>
+    {
+      return total + current;
+    }, 0
+    );
+    return product;
+  } else
+    return 0;
 };
 
-const multiply = function() {
+const multiply = function (array)
+{
+  const product = array.reduce((total, current) =>
+  {
+    if (array.length > 0)
+    {
+      return total * current;
+    };
+  }, 1
+  );
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b)
+{
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number)
+{
+  let factorial = 0;
+
+  if (number > 2)
+  {
+    let spreed = [];
+    for (let index = number; index >= 1; index--)
+    {
+      spreed.push(index);
+    }
+    factorial = spreed.reduce((total, current) =>
+    {
+      return total * current;
+    },
+      1
+    );
+
+  } else if (number <= 2 && number !== 0)
+  {
+    factorial = number;
+  } else
+    factorial = 1;
+
+  return factorial;
+
 };
 
 // Do not edit below this line
