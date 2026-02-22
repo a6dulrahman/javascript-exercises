77df308bdd8b89429b98eb8fba4526f5def9ff43
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-22 10:26:30 +0200

    Finish 15_getTheTitles
    ☺️

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function (array)
+{
+    let accumulator = [];
 
+    for (let index = 0; index < array.length; index++)
+    {
+        accumulator.push(array[index].title);
+    }
+
+    return accumulator;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
