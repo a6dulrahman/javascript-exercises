eee2a8d5c6eece888d7b9a6e4549079759b1f65c
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-22 10:16:14 +0200

    Finish 14_fibonacci
    ☺️

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
 
+const fibonacci = function (number)
+{
+    let arr = [0, 1, ];
+
+    if (number == 0) return 0;
+    if (number < 0) return "OOPS";
+    
+    for (let index = 2; index <= number; index++)
+    {
+        let fibonacciValue = (index - 1) + (index - 2);
+        if (fibonacciValue >= 0)
+        {
+            arr[index] = arr[index -1] + arr[index - 2];
+        }
+
+    };
+
+    return arr[number];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
