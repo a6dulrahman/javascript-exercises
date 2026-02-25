71364af3eb2ebe6911dbd0ab0985bbfad99e9533
a6dulrahman <259852835+a6dulrahman@users.noreply.github.com>
2026-02-25 20:57:09 +0200

    Finish 16_findTheOldest
    ☺️

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (array)
+{
+    function calculateAge(obj)
+    {
+        if ("yearOfDeath" in obj)
+        {
+            return obj.yearOfDeath - obj.yearOfBirth;
+        } else
+            return (new Date().getFullYear()) - obj.yearOfBirth;
+
+    }
+
+    const obj = array.reduce((total, current) =>
+    {
+        current["age"] = calculateAge(current);
+        total.push(current);
+        return total;
+    }, []
+    ).sort((a, b) =>
+    {
+        return b.age - a.age;
+    }
+    );
+
+    return obj[0];
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
